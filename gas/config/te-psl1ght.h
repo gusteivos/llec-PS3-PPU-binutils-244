1b3a3d5653b8e0a7a65fa8beca86210019bea1c6
Gustavo Ramos Carvalho <gc5142387@gmail.com>
2025-10-22 21:09:13 -0300

    initial work for PSL1GHT target

---
+++ gas/config/te-psl1ght.h
@@ -0,0 +1,12 @@
+/* ./te-generic.h */
+
+#define TE_PSL1GHT 1
+
+#define LOCAL_LABELS_DOLLAR 1
+#define LOCAL_LABELS_FB 1
+
+#ifdef   OBJ_HEADER
+#include OBJ_HEADER
+#else
+#include "obj-format.h"
+#endif
